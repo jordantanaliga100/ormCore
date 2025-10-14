a66aeaa9386ff3d89ef7ec192b0aba7b8c1a641a
tanaliga100 <jordantanaliga@gmail.com>
2025-10-14 16:06:27 +0800

    chore(config): added 3 db's | mongo, mysql, postgre | runs in docker

--- typeorm/inventory/src/config/data-source.ts
+++ typeorm/inventory/src/config/data-source.ts
@@ -1,34 +1,14 @@
-import { config } from "dotenv";
-import { DataSource } from "typeorm";
-import { Category } from "../entities/category.entity";
-import { Product } from "../entities/product.entity";
-import { UserProfile } from "../entities/profile.entity";
-import { SaleItem } from "../entities/sale-item.entity";
-import { Sale } from "../entities/sale.entity";
-import { StockHistory } from "../entities/stock-history.entity";
-import { Supplier } from "../entities/supplier.entity";
-import { Tag } from "../entities/tag.entity";
-import { User } from "../entities/user.entity";
-config();
+import { MongoDataSource } from "../db/mongodb";
+import { MysqlDataSource } from "../db/mysql";
+import { PostgresDataSource } from "../db/postgres";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: process.env.NODE_ENV !== "production",
-  logging: true,
-  entities: [
-    User,
-    Supplier,
-    Product,
-    Category,
-    Sale,
-    SaleItem,
-    StockHistory,
-    Tag,
-    UserProfile,
-  ],
-});
+export const connectDataSource = async () => {
+  try {
+    await PostgresDataSource.initialize(); //🔥
+    await MysqlDataSource.initialize(); //🔥
+    await MongoDataSource.initialize(); //🔥
+    console.log("✅ ALL Data Source has been initialized!");
+  } catch (error: any) {
+    console.error("❌ Error during Data Source initialization:", error);
+  }
+};
--- typeorm/inventory/src/index.ts
+++ typeorm/inventory/src/index.ts
@@ -1,12 +1,12 @@
 import app from "./app";
-import { connectPostgres } from "./db/postgres";
+import { connectDataSource } from "./config/data-source";
 
 // SERVER INSTANCE
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectPostgres();
+    await connectDataSource();
     app.listen(port, () => {
       console.log("Server started at " + port + " and connected to DB !!");
     });
